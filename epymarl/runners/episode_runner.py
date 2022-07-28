57eaf6129c64b85120a28bfd003e9af1619da8a1
James Ault <jault@tamu.edu>
2022-07-28 11:24:15 -0600

    removing prints from runner

--- epymarl/runners/episode_runner.py
+++ epymarl/runners/episode_runner.py
@@ -46,7 +46,6 @@ class EpisodeRunner:
         self.t = 0
 
     def run(self, test_mode=False):
-        print("RUNNING EPISODE")
         self.reset()
 
         terminated = False
@@ -98,10 +97,8 @@ class EpisodeRunner:
         cur_stats["n_episodes"] = 1 + cur_stats.get("n_episodes", 0)
         cur_stats["ep_length"] = self.t + cur_stats.get("ep_length", 0)
 
-        print('TEST MODE', test_mode)
         if not test_mode:
             self.t_env += self.t
-            print(self.t_env, self.t)
 
         cur_returns.append(episode_return)
 
